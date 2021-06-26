c52c5f189f26435fef157c10f57384a8aab77425
lakshmipathi.palla <lakshmipathi.palla@lakshmipathi>
2021-06-26 14:53:03 +0530

    fundanalyzer

---
+++ financialspendanalyzer/src/main/java/com/financial/analyzer/FinancialspendanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.financial.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialspendanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialspendanalyzerApplication.class, args);
+	}
+
+}
---
+++ financialspendanalyzer/src/test/java/com/financial/analyzer/FinancialspendanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialspendanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
